9a17e889b655d72c659d8ab046c19eec5341044b
P-ano-past <marcsalaver@gmail.com>
2020-05-24 16:56:11 -0700

    organization for OAuth2

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8081;
+const session = require('express-session');
+const authRoute = require('./routes/auth');
+const discordStrategy = require('./stratagies/discordstrategy');
+const passport = require('passport');
+
+app.use(session({
+	secret: 'some random secret',
+	cookie: {
+		maxAge: 60000 * 60 * 24,
+	},
+	saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRoute);
+
+app.listen(PORT, () => {
+	console.log(`now listening to requests on port ${PORT}`);
+});
+
+// https://www.youtube.com/watch?v=r_tXkzBgmFc
