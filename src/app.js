643f18cc693d85c100a1809558c9f4c90c99ebcd
Evan Wenzel <xevanx2002@gmail.com>
2020-05-27 17:40:44 -0700

    everything

--- src/app.js
+++ src/app.js
@@ -7,6 +7,28 @@ const authRoute = require('../src/routes/auth');
 const discordStrategy = require('../src/strategies/discordstrategy');
 const passport = require('passport');
 
+app.engine("handlebars", exphbs({ defaultLayout: "commands" }));
+app.set("view engine", "handlebars");
+
+var commands = [
+	{
+		commandTitle: "!join",
+		commandUse: "To test if the bot can join the voice channel for music use"
+	}, {
+		commandTitle: "!leave",
+		commandUse: "To kick the bopt from a voice channel whenever needed"
+	}, {
+		commandTitle: "!play",
+		comamndUse: "Using this command following a Youtube url makes the bot join the voice channel to listen to audio from the video provided"
+	}, {
+		commandTitle: "!meme",
+		commandUse: "This command makes the bot randomly pull a meme from reddit and posts it into the chat"
+	}, {
+		commandTitle: "!command",
+		commandUse: "This command brings up a simple list of commands into the chat for everyone's use"
+	}
+]
+
 app.use(session({
 	secret: 'some random secret',
 	cookie: {
